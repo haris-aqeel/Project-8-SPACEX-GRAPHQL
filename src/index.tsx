3b91836e78cec9fd4eafc621acf0174a330bea35
Haris Aqeel <harisaqeel.2001@gmail.com>
2020-11-03 23:33:01 +0500

    Completed Styling

--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import ApolloClient from 'apollo-boost';
-import { ApolloProvider } from 'react-apollo';
-import { ApolloProvider as ApolloHooksProvider } from 'react-apollo-hooks';
+import { ApolloProvider, InMemoryCache, ApolloClient } from '@apollo/client';
 import './index.css';
 import App from './App';
 
 const client = new ApolloClient({
   uri: 'https://spacexdata.herokuapp.com/graphql',
+  cache: new InMemoryCache()
 });
 
 ReactDOM.render(
   <ApolloProvider client={client}>  
-  {/* <ApolloHooksProvider client={client}> */}
+ 
       <App />
-  {/* </ApolloHooksProvider> */}
   </ApolloProvider>,
   document.getElementById('root'),
 );
