006e1d0ee76b2a04a32bed970a5e66f653601566
Haris Aqeel <harisaqeel.2001@gmail.com>
2020-11-03 22:40:50 +0500

    Completed Functionalities of Project

---
+++ src/components/LaunchDetails/index.tsx
@@ -0,0 +1,33 @@
+  
+import * as React from 'react';
+import { useLaunchProfileQuery } from '../../generated/graphql';
+import LaunchDetails from './LaunchDetails';
+
+interface OwnProps {
+  id: number;
+}
+
+const LaunchProfileContainer = ({ id }: OwnProps) => {
+  const { data, error, loading, refetch } = useLaunchProfileQuery({
+    variables: { id: String(id) },
+  });
+  React.useEffect(() => {
+    refetch();
+  }, [id]);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>ERROR</div>;
+  }
+
+  if (!data) {
+    return <div>Select a flight from the panel</div>;
+  }
+
+  return <LaunchDetails data={data} />;
+};
+
+export default LaunchProfileContainer;
